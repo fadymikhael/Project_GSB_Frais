b370ee6dbc586da960e0e5290b5fc1a0d62b8f5a
Maitriya <maitriya@outlook.fr>
2024-01-16 15:37:43 +0100

    Premier commit

---
+++ app/Http/Controllers/protoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class protoController extends Controller
+{
+    function voirDernierFrais()
+    {
+       return view('proto');
+    
+
+    }
+}
